1a5f29652c631cded86027192931626eadb083a9
Michael <100490812+DebSol11@users.noreply.github.com>
2026-05-14 19:04:26 +0200

    Code along the YouTube tutorial

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ app.get('/', (req, res) => {
     res.render("index", {text777: "Word"})
 })
 
+// group all routes in their router files
+// import the users router file
+const userRouter = require('./routes/users')
+
+// link up the routes from our users.js file
+// linking a route to a particular path
+app.use('/users', userRouter)
+
 app.listen(3000);
